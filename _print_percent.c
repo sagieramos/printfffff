474e5be1f6bf20de9044f0f7330f0974f4c4c4dd
Fashina Segun <fashina.segun25@gmail.com>
2022-08-09 18:26:00 +0100

    adding percent

---
+++ _print_percent.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * print_percent - prints persentage
+ *
+ * Return: 1
+ */
+int print_percent(void)
+{
+	_putchar('%');
+	return (1);
+}
